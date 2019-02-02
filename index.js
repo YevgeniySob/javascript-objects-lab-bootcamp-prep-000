6f4aeae3c79d5b8e65e0a046cfff958b17092f49
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 20:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
+  var my_obj = Object.assign({}, object)
+  my_obj[key] = value
+  return my_obj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
